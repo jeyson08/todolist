64c93d9061984100d226f133d2941fd3995b23ac
jeyson08 <jeyson.boursault@gmail.com>
2024-05-24 11:42:54 +0200

    logique ajout et suppression

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
+const input = document.getElementById("todo");
+const ul = document.querySelector("ul");
 
+// console.log(lis);
+
+document.addEventListener("keypress", (e) => {
+  if (e.key === "Enter" && input.value !== "") {
+    const li = document.createElement("li");
+    li.textContent = input.value;
+    ul.appendChild(li);
+    input.value = "";
+    // input.blur(); Enleve le focus
+    li.addEventListener("click", (e) => {
+      ul.removeChild(e.target);
+    });
+  }
+});
